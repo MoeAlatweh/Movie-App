844c367a809ae08cd99197e2c25fd336e8b39b6a
MoeAlatweh <moemenatweh@hotmail.com>
2020-09-07 22:09:02 -0400

    Update some stuff for deploying.

--- cp_django_movie_app/settings.py
+++ cp_django_movie_app/settings.py
@@ -121,10 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_URL = '/static/'
-
-STATIC_URL = 'C:\Users\Alatweh\Desktop\Python Course\github\cp-movie-app\movies\static\movies'
+STATIC_URL = '/static/'
 
 ##add lines below to deploy app on horuko
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'movies/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'C:\Users\Alatweh\Desktop\Python Course\github\cp-movie-app\movies\static\movies')]
